cfe660bf3fb52b22c3b2ec7e54e7877a91f04310
Richard Bateman <taxilian@gmail.com>
2015-12-21 11:13:01 -0700

    Minor refactor to use setImmediate to provide shallow stacks for profiler calls

--- index.js
+++ index.js
@@ -10,32 +10,48 @@ var verbose = false;
 
 var nextId = 0;
 
+function endProfiling(id, cb) {
+    var p = profiler.stopProfiling(id);
+    if (verbose) { console.log("CPU Profiling complete"); }
+    setImmediate(cb, p);
+}
+
 function runProfile(seconds, cb) {
     if (verbose) { console.log("Starting CPU profiling for", seconds, "seconds."); }
     var id = "cpu_" + nextId++;
-    profiler.startProfiling(id);
+    setImmediate(profiler.startProfiling, id);
     var done = false;
 
-    setTimeout(function endProfiling() {
-        done = true;
-        var p = profiler.stopProfiling(id);
-        if (verbose) { console.log("CPU Profiling complete"); }
-        cb(p);
-    }, 1000 * seconds);
+    setTimeout(endProfiling, 1000 * seconds, id, cb);
+}
+
+function releaseProfile(p) {
+    if (verbose) { console.log("Releasing CPU profile"); }
+    p.delete();
+}
+function releaseSnapshot(s) {
+    if (verbose) { console.log("Releasing heap snapshot"); }
+    s.delete();
+}
+
+function exportHeapSnapshot(req, res) {
+    var snapshot = profiler.takeSnapshot();
+    var fname = new Date().getTime() + ".heapsnapshot";
+    res.writeHead('200', {
+        "Content-Type": "application/json",
+        "Content-Disposition": 'attachment; filename="' + fname + '"'
+    });
+    if (verbose) { console.log("Generating heap snapshot"); }
+    snapshot.export().pipe(res).on('finish', function() {
+        setImmediate(releaseSnapshot, snapshot);
+    });
 }
 
 var routes = {
     'currentHeap.heapsnapshot': function getCurrentHeap(req, res) {
-        var snapshot = profiler.takeSnapshot();
-        var fname = new Date().getTime() + ".heapsnapshot";
-        res.writeHead('200', {
-            "Content-Type": "application/json",
-            "Content-Disposition": 'attachment; filename="' + fname + '"'
-        });
-        if (verbose) { console.log("Generating heap snapshot"); }
-        snapshot.export().pipe(res).on('finish', function() {
-            profiler.deleteAllSnapshots();
-        });
+        // We want to keep the stack as shallow as possible when this happens to avoid any
+        // potential conflicts (yes, issues were observed before we did this)
+        setImmediate(exportHeapSnapshot, req, res);
     },
     'profile.cpuprofile': function getCPUProfile(req, res, query) {
         var seconds = query.seconds || 60;
@@ -48,8 +64,7 @@ var routes = {
 
         runProfile(seconds, function sendProfileToBrowser(p) {
             p.export().pipe(outputStream).on('finish', function deleteProfile() {
-                //p.delete();
-                profiler.deleteAllProfiles();
+                setImmediate(releaseProfile, p);
             });
         });
     },
@@ -68,7 +83,7 @@ var routes = {
                 if (verbose) { console.log("Sending flamegraph to caller"); }
                 var svg = flamegraph([ pText ], {inputtype:'cpuprofile'});
                 res.end(svg);
-                profiler.deleteAllProfiles();
+                setImmediate(releaseProfile, p);
             });
         });
     }
@@ -78,7 +93,7 @@ function measureServer(config) {
     if (config.verbose) { verbose = config.verbose; }
     config = config || {};
     var port = config.port || 12345;
-    if (verbose) { console.log("Profiling helper listening on port", port); }
+    console.warn(" >> measurestuff profiling helper listening on port", port);
     var server = http.createServer().listen(port);
     server.on('request', function measureServerOnRequest(req, res) {
         if (verbose) { console.log("Profiling helper HTTP request to ", req.method, req.url); }
