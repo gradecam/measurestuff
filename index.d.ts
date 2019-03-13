51e6423f6692ca95f6c59733e4879e6c9bf8d912
Richard Bateman <taxilian@gmail.com>
2019-03-12 21:44:30 -0600

    Add typescript definition (untested)

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import { Server } from "http";
+
+declare module 'measurestuff' {
+    interface MeasureStuffConfig {
+        verbose?: boolean; // Default false
+        port?: number; // Default 12345
+        seconds?: number; // default 60
+    }
+    interface MeasureStuffServer {
+        (config?: MeasureStuffConfig) : void;
+        close(cb?: (err?: Error) => void) : Server;
+    }
+}
