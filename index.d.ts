512ac916b6c2a32925cc446e883346650ba54690
Richard Bateman <richardb@gradecam.com>
2021-05-27 23:26:02 -0600

    Refactor typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,14 @@
 import { Server } from "http";
 
-declare module 'measurestuff' {
-    interface MeasureStuffConfig {
-        verbose?: boolean; // Default false
-        port?: number; // Default 12345
-        seconds?: number; // default 60
-    }
-    interface MeasureStuffServer {
-        (config?: MeasureStuffConfig) : void;
-        close(cb?: (err?: Error) => void) : Server;
-    }
-
-    const server: MeasureStuffServer;
-    export = server;
+export interface MeasureStuffConfig {
+    verbose?: boolean; // Default false
+    port?: number; // Default 12345
+    seconds?: number; // default 60
 }
+export interface MeasureStuffServer {
+    (config?: MeasureStuffConfig) : void;
+    close(cb?: (err?: Error) => void) : Server;
+}
+
+declare const server: MeasureStuffServer;
+export default server;
